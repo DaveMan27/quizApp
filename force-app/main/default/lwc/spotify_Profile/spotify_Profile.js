87c100bb26e67044c81fee6efa4c3de18b1f8b95
DAVE LAWRENCE <davidl@techunity.co.il>
2024-02-26 14:41:49 +0200

    added component to front end

--- force-app/main/default/lwc/spotify_Profile/spotify_Profile.js
+++ force-app/main/default/lwc/spotify_Profile/spotify_Profile.js
@@ -1,3 +1,65 @@
 import { LightningElement } from 'lwc';
+import getUserProfile from '@salesforce/apex/Spotify_lwcController.getUserProfile';
+import getUserPlaylists from '@salesforce/apex/Spotify_lwcController.getUserPlaylists';
 
-export default class Spotify_Profile extends LightningElement {}
+export default class Spotify_Profile extends LightningElement {
+
+    userProfile             = {};
+    userPlaylists           = {};
+    simplifiedPlaylistArray = [];
+    showPlaylists            = false;
+    
+    columns = [
+        { label: 'Name', fieldName: 'name' },
+        { label: 'Link', fieldName: 'link', type: 'url' }        
+    ];
+
+    async handleGetProfile() {
+        try {
+            this.userProfile = await getUserProfile();
+            this.userProfile = JSON.parse(this.userProfile);
+            console.log(this.userProfile);
+        } catch (error) {
+            console.error('Error getting user profile', error);
+        }
+    }
+
+    async handleGetPlaylists() {
+        try {
+            this.userPlaylists = await getUserPlaylists();
+            this.userPlaylists = JSON.parse(this.userPlaylists);
+            console.log(this.userPlaylists);
+        } catch (error) {
+            console.error('Error getting user profile', error);
+        }
+
+        this.handlePlaylistArray();
+        if (this.simplifiedPlaylistArray.length > 0) {
+            {
+                this.showPlaylists = true;
+            }
+        }
+    }
+
+    handlePlaylistArray()
+    {
+        this.simplifiedPlaylistArray = this.userPlaylists.items.map(playlist => ({
+            link: playlist.href,
+            name: playlist.name,
+            id: playlist.snapshot_id,
+            image: playlist.images[0].url
+        }))
+        console.log(this.simplifiedPlaylistArray);
+    }
+
+    handlePlaylistClick(event) {
+        const itemId = event.currentTarget.dataset.id;
+        console.log(`Clicked item ID: ${itemId}`);
+    }
+
+   
+
+}
+
+
+    

a45157cc159732983e77cdb67b4b6513fb05bc5c
DAVE LAWRENCE <davidl@techunity.co.il>
2024-02-29 12:14:34 +0200

    fixes successful

--- force-app/main/default/lwc/spotify_Profile/spotify_Profile.js
+++ force-app/main/default/lwc/spotify_Profile/spotify_Profile.js
@@ -25,7 +25,7 @@ export default class Spotify_Profile extends LightningElement {
     // Fetch and process user profile data
     async handleGetProfile() {
         try {
-            this.userProfile = await getUserProfile({ username: username});
+            this.userProfile = await getUserProfile({ username: this.username});
             this.userProfile = JSON.parse(this.userProfile);
             console.log(this.userProfile);
         } catch (error) {
@@ -36,7 +36,7 @@ export default class Spotify_Profile extends LightningElement {
     // Fetch and process user playlists
     async handleGetPlaylists() {
         try {
-            this.userPlaylists = await getUserPlaylists({ username: username});
+            this.userPlaylists = await getUserPlaylists({ username: this.username});
             this.userPlaylists = JSON.parse(this.userPlaylists);
             console.log(this.userPlaylists);
         } catch (error) {
@@ -50,13 +50,14 @@ export default class Spotify_Profile extends LightningElement {
 
     // Process playlists data to simplify and prepare for display
     handlePlaylistArray() {
-        this.simplifiedPlaylistArray = this.userPlaylists.items.map(playlist => ({
-            link: playlist.href,
-            name: playlist.name,
-            id: playlist.id,
-            image: playlist.images[0].url
-        }))
-        console.log(this.simplifiedPlaylistArray);
+        this.simplifiedPlaylistArray = this.userPlaylists.items.map(({ href, name, id, images }) => ({
+            link: href,
+            name,
+            id,
+            image: images.length > 0 ? images[0].url : 'defaultImageUrl' // Assuming 'defaultImageUrl' is a placeholder you have for playlists without an image
+          }));
+          
+          console.log(JSON.parse(JSON.stringify(this.simplifiedPlaylistArray)));
     }
 
     async handlePlaylistSelect(event) {
@@ -65,35 +66,35 @@ export default class Spotify_Profile extends LightningElement {
         const itemId = event.detail;
         console.log(`Clicked item ID: ${itemId}`);
         try {
-            let playlist = await openPlaylist({ playListID: itemId });
+            /*let playlist = await openPlaylist({ playListID: itemId });
                 playlist = JSON.parse(playlist);            
-            console.log('Opening playlist: ', playlist);
+            console.log('Opening playlist: ', playlist);*/
             let tracksList = await getPlaylistTracks({ playListID: itemId });
                 tracksList = JSON.parse(tracksList);
             
             console.log('Tracks: ', tracksList);
             if (tracksList.total > 0) {
-                this.simplifiedTrackArray = tracksList.items.map(item => ({
-                    name            : item.track.name,
-                    artists         : item.track.artists.map(artist => artist.name),
-                    artists_nonArray: item.track.artists.map(artist => artist.name).join(', '),
-                    album           : item.track.album.name,
-                    id              : item.track.id,
-                    external_link   : item.track.external_urls.spotify,
-                    href            : item.track.href
-                }));
-                  // tracksList.items.forEach(item => {
-                  //     getTrackAudioAnalysis({ trackID: item.track.id })
-                  //        .then(trackAnalysis => {
-                  //             trackAnalysis = JSON.parse(trackAnalysis);
-                  //            console.log('Track analysis: ', trackAnalysis);
-                  //            this.multiTrackAnalysisArray.push(trackAnalysis);                             
-                  //         })
-                  //        .catch(error => {
-                  //             console.error('Error getting track analysis', error);
-                  //         })
-                  // })
-                this.generateAudioAnalysisArray(tracksList);
+                this.simplifiedTrackArray = tracksList.items.map(({ track }) => {
+                    const { name, artists, album, id, external_urls: { spotify: external_link }, href } = track;
+  // Combine the artist names in a single iteration
+                    const artistNames = artists.map(artist => artist.name);                    
+                    const artists_nonArray = artistNames.join(', ');
+                    return {
+                        name,
+                        artists: artistNames,
+                        artists_nonArray,
+                        album: album.name,
+                        id,
+                        external_link,
+                        href
+                    };
+                });
+
+                
+
+               
+                
+                  /*this.generateAudioAnalysisArray(tracksList);
                   
                 console.log('Track analysis array: ', this.multiTrackAnalysisArray);
                 this.trackIds = this.simplifiedTrackArray.map(track => track.id).join(',');
@@ -101,9 +102,11 @@ export default class Spotify_Profile extends LightningElement {
                 let trackFeatures = await getMultiTrackAudioFeatures({ trackIDs: this.trackIds });
                 trackFeatures = JSON.parse(trackFeatures);
                 console.log('Track features: ', trackFeatures);
-
+                                                                */
                 this.showTracks = true;
                 console.log(JSON.parse(JSON.stringify(this.simplifiedTrackArray)));
+                this.generateAudioAnalysisArray(tracksList);
+                console.log('Multi item analysis array: ', this.multiTrackAnalysisArray);
             }
             console.log('Tracks: ', tracksList);
         } catch (error) {
@@ -128,8 +131,7 @@ export default class Spotify_Profile extends LightningElement {
         tracksList.items.forEach(item => {
             getTrackAudioAnalysis({ trackID: item.track.id })
                .then(trackAnalysis => {
-                    trackAnalysis = JSON.parse(trackAnalysis);
-                   console.log('Track analysis: ', trackAnalysis);
+                   trackAnalysis = JSON.parse(trackAnalysis);
                    this.multiTrackAnalysisArray.push(trackAnalysis);                             
                 })
                .catch(error => {
@@ -138,13 +140,6 @@ export default class Spotify_Profile extends LightningElement {
         })
         console.log('Working function');
     }
-
-
-
-
-
-
-
 }
 
 

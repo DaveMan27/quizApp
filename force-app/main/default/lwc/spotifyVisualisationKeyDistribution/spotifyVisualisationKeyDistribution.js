50403afb3e93b90ef80033d2a9dc942ad66e8106
DAVE LAWRENCE <davidl@techunity.co.il>
2024-02-29 16:17:46 +0200

    working visualization on custom data

--- force-app/main/default/lwc/spotifyVisualisationKeyDistribution/spotifyVisualisationKeyDistribution.js
+++ force-app/main/default/lwc/spotifyVisualisationKeyDistribution/spotifyVisualisationKeyDistribution.js
@@ -1,120 +1,115 @@
-import { LightningElement, api } from "lwc";
-import { ShowToastEvent } from "lightning/platformShowToastEvent";
-import { loadScript, loadStyle } from "lightning/platformResourceLoader";
-import D3 from "@salesforce/resourceUrl/d3_min";
-import DATA from "@salesforce/resourceUrl/data";
-import STYLES from "@salesforce/resourceUrl/styles";
+import { LightningElement } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import { loadScript, loadStyle } from 'lightning/platformResourceLoader';
+import D3 from '@salesforce/resourceUrl/d3_min';
+import DATA from '@salesforce/resourceUrl/data'; // Ensure this points to a valid URL
+import STYLES from '@salesforce/resourceUrl/styles';
 
 export default class SpotifyVisualisationKeyDistribution extends LightningElement {
-
-   @api svgWidth  = 928;
-   @api svgHeight = 500; 
-
-  d3Initialized = false;
+    svgWidth = 928;
+    svgHeight = 500;
+    d3Initialized = false;
 
     renderedCallback() {
-   
-    if (this.d3Initialized) {
-        return;
-    }
-    this.d3Initialized = true;
+        if (this.d3Initialized) {
+            return;
+        }
 
-      // Load D3 and styles first
         Promise.all([loadScript(this, D3), loadStyle(this, STYLES)])
             .then(() => {
-                                    // After D3 and styles are loaded, fetch your data
-                return fetch(DATA)  // Assuming DATA is a URL string
+                // Assuming DATA is a URL to your data resource
+                fetch(DATA)
                     .then(response => {
                         if (!response.ok) {
                             throw new Error('Network response was not ok');
                         }
-                        return response.json();  // Parse JSON data from the response
+                        return response.json();
                     })
                     .then(data => {
-                                                // Now that we have the data, initialize D3 with it
-                        this.initializeD3(data);  // Pass data to your D3 initialization function
+                        this.initializeD3(data);
+                    })
+                    .catch(error => {
+                        this.handleError(error);
                     });
             })
-            .catch((error) => {
-                console.error("Error loading resources or fetching data", error);
-                this.dispatchEvent(
-                    new ShowToastEvent({
-                        title: "Error",
-                        message: error.message,
-                        variant: "error",
-                    }),
-                );
+            .catch(error => {
+                this.handleError(error);
             });
-    }   
 
-
-
-
-  initializeD3(data) {
-      // Example adopted from https://bl.ocks.org/mbostock/2675ff61ea5e063ede2b5d63c08020c7
-    
-   
-      const svg = d3.select(this.template.querySelector('svg'));
-      const margin = { top: 20, right: 0, bottom: 30, left: 40 };
-      const width = this.svgWidth - margin.left - margin.right;
-      const height = this.svgHeight - margin.top - margin.bottom;
-
-      // Scales and axes setup...
-      const x = d3.scaleBand()
-          .rangeRound([0, width])
-          .padding(0.1)
-          .domain(data.map(d => d.key));
-
-      const y = d3.scaleLinear()
-          .rangeRound([height, 0])
-          .domain([0, d3.max(data, d => d.frequency)]);
-
-      // Append SVG and groups, applying transformations...
-      svg.attr('viewBox', `0 0 ${this.svgWidth} ${this.svgHeight}`)
-          .append('g')
-          .attr('transform', `translate(${margin.left},${margin.top})`);
-      
-      // Example: Appending bars...
-      svg.selectAll('.bar')
-          .data(data)
-          .enter().append('rect')
-          .attr('class', 'bar')
-          .attr('x', d => x(d.key))
-          .attr('y', d => y(d.frequency))
-          .attr('width', x.bandwidth())
-          .attr('height', d => height - y(d.frequency));
-
-  // Append the axes.
-  svg.append("g")
-      .attr("class", "x-axis")
-      .attr("transform", `translate(0,${this.svgHeight - margin.bottom})`)
-      .call(xAxis);
-
-  svg.append("g")
-      .attr("class", "y-axis")
-      .attr("transform", `translate(${margin.left},0)`)
-      .call(d3.axisLeft(y))
-      .call(g => g.select(".domain").remove());
-
-  return svg.node();
-
-  function zoom(svg) {
-    const extent = [[margin.left, margin.top], [this.svgWidth - margin.right, this.svgHeight - margin.top]];
-
-    svg.call(d3.zoom()
-        .scaleExtent([1, 8])
-        .translateExtent(extent)
-        .extent(extent)
-        .on("zoom", zoomed));
-
-    function zoomed(event) {
-      x.range([margin.left, this.svgWidth - margin.right].map(d => event.transform.applyX(d)));
-      svg.selectAll(".bars rect").attr("x", d => x(d.key)).attr("width", x.bandwidth());
-      svg.selectAll(".x-axis").call(xAxis);
+        this.d3Initialized = true;
     }
-  }
+
+    initializeD3(data) {
+        const svgElement = this.template.querySelector('svg');
+        const svg = d3.select(svgElement);
+        const margin = { top: 20, right: 20, bottom: 30, left: 40 };
+        const width = this.svgWidth - margin.left - margin.right;
+        const height = this.svgHeight - margin.top - margin.bottom;
+        const g = svg.append('g').attr('transform', `translate(${margin.left},${margin.top})`);
     
-  }
+        // Scales
+        const x = d3.scaleBand()
+            .rangeRound([0, width])
+            .padding(0.1)
+            .domain(data.map(d => d.key));
+        const y = d3.scaleLinear()
+            .rangeRound([height, 0])
+            .domain([0, d3.max(data, d => d.frequency)]);
+    
+        // Zoom behavior
+        const zoom = d3.zoom()
+            .scaleExtent([1, 10])
+            .extent([[margin.left, 0], [width - margin.right, height]])
+            .on('zoom', zoomed);
+    
+        // Apply the zoom behavior to the SVG element
+        svg.call(zoom);
+    
+        // Append bars
+        g.selectAll('.bar')
+            .data(data)
+            .enter().append('rect')
+            .attr('class', 'bar')
+            .attr('x', d => x(d.key))
+            .attr('y', d => y(d.frequency))
+            .attr('width', x.bandwidth())
+            .attr('height', d => height - y(d.frequency));
+    
+        // Append axes
+        g.append('g')
+            .attr('class', 'axis axis--x')
+            .attr('transform', `translate(0,${height})`)
+            .call(d3.axisBottom(x));
+        g.append('g')
+            .attr('class', 'axis axis--y')
+            .call(d3.axisLeft(y));
+    
+        function zoomed(event) {
+            // Create new scale objects based on event
+            const newX = event.transform.rescaleX(x);
+            const newY = event.transform.rescaleY(y);
+    
+            // Update axes
+            g.select('.axis--x').call(d3.axisBottom(newX));
+            g.select('.axis--y').call(d3.axisLeft(newY));
+    
+            // Update bars
+            g.selectAll('.bar')
+                .attr('x', d => newX(d.key))
+                .attr('width', newX.bandwidth())
+                .attr('y', d => newY(d.frequency))
+                .attr('height', d => height - newY(d.frequency));
+        }
+    }
+    
+    handleError(error) {
+        console.error('Error:', error);
+        this.dispatchEvent(
+            new ShowToastEvent({
+                title: 'Error loading D3 or data',
+                message: error.message,
+                variant: 'error',
+            }),
+        );
+    }
 }
-
-
